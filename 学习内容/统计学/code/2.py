bcb61e1d3609672d88914749afdd629d1da8aa5f
fengkai <fengkai06@meituan.com>
2019-11-13 19:15:08 +0800

    增加描述性数据实践图片

--- 学习内容/统计学/code/2.py
+++ 学习内容/统计学/code/2.py
@@ -2,8 +2,20 @@
 import pandas as pd
 import numpy as np
 
+from matplotlib import pyplot
+
+
+def draw_box(heights):
+    # 创建箱形图
+    # 第一个参数为待绘制的定量数据
+    pyplot.boxplot([heights], labels=['data'])
+    # 第二个参数为数据的文字说明
+    pyplot.title('The data distribution ')
+    pyplot.show()
+
+
 if __name__ == '__main__':
-    path = '实践一.xlsx'
+    path = '../file/实践一.xlsx'
     data = pd.read_excel(path, encoding='utf-8')
     print("众数是：%s" % data['data'].mode().values)
     print("中位数是：%s" % data['data'].median())
@@ -14,6 +26,10 @@ if __name__ == '__main__':
     print("加权平均数是：%s" % ((da['data'] * da['count']).sum() / da['count'].sum()))
     # data['mul']=data['data'].cumprod().head(300)
     # print("几何平均数是：%s" %)
-    print("极差是：%s"%(data['data'].max()-data['data'].min()))
-    print("方差是：%s"%(data['data'].var()))
-    print("标准差：%s"%data['data'].std())
+    print("极差是：%s" % (data['data'].max() - data['data'].min()))
+    print("方差是：%s" % (data['data'].var()))
+    print("标准差：%s" % data['data'].std())
+    print("偏度：%s" % data['data'].skew())
+    print("峰度：%s" % data['data'].kurt())
+
+    draw_box(data['data'])
